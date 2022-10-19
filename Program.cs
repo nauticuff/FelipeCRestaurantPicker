9e5d58b4bc223f53d7594af364f26fd26ab479fc
nauticuff <felipecarlos200x@gmail.com>
2022-10-19 15:46:11 -0700

    changed code explanation

--- Program.cs
+++ Program.cs
@@ -1,8 +1,8 @@
 ﻿/*
 Carlos Felipe
 10/19/22
+Mini Challenge #9 - Restaurant Picker
 This code prints out a randomized restaurant from a restaurant category chosen by the user. 
-DONE
 */
 
 Console.Clear();
